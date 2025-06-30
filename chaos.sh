9f7929902b8f3b81544ece3a9f070f810917b671
Neslihan Bükte <neslihanbukte20@gmail.com>
2025-06-30 22:09:27 +0300

    correct label selector in chaos.sh

--- chaos.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-while true
-do
-  POD=$(kubectl get pods -l app=my-app -o jsonpath="{.items[0].metadata.name}")
-  echo "Deleting pod: $POD"
-  kubectl delete pod $POD
-  sleep 10
-done
